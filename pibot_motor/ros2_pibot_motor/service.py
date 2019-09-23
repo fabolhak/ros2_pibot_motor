8b2904dc96b44f49acdb331c4f9894e6f3570779
FaHa <dev-faha@t-online.de>
2019-09-23 11:16:45 +0200

    disable all gpio related stuff

--- pibot_motor/ros2_pibot_motor/service.py
+++ pibot_motor/ros2_pibot_motor/service.py
@@ -14,7 +14,7 @@
 
 from std_srvs.srv import Trigger
 import rclpy
-import RPi.GPIO as GPIO # Import the GPIO Library
+#import RPi.GPIO as GPIO # Import the GPIO Library
 import time # Import the Time library
 
 
@@ -27,20 +27,20 @@ def triggerMotor(request, response):
     g_node.get_logger().info('Trigger Motor!')
 
     # Turn the right motor forwards
-    GPIO.output(9, 0)
-    GPIO.output(10, 1)
+   # GPIO.output(9, 0)
+   # GPIO.output(10, 1)
 
     # Turn the left motor forwards
-    GPIO.output(7, 0)
-    GPIO.output(8, 1)
+   # GPIO.output(7, 0)
+   # GPIO.output(8, 1)
     # Wait for 1 seconds
     time.sleep(1)
 
     # Turn all motors off
-    GPIO.output(7, 0)
-    GPIO.output(8, 0)
-    GPIO.output(9, 0)
-    GPIO.output(10, 0)
+   # GPIO.output(7, 0)
+   # GPIO.output(8, 0)
+   # GPIO.output(9, 0)
+   # GPIO.output(10, 0)
 
     response.success = True
     response.message = "Sucess!"
@@ -53,18 +53,18 @@ def main(args=None):
     rclpy.init(args=args)
 
     # Set the GPIO modes
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+  #  GPIO.setmode(GPIO.BCM)
+  #  GPIO.setwarnings(False)
     # Set the GPIO Pin mode
-    GPIO.setup(7, GPIO.OUT)
-    GPIO.setup(8, GPIO.OUT)
-    GPIO.setup(9, GPIO.OUT)
-    GPIO.setup(10, GPIO.OUT)
+  #  GPIO.setup(7, GPIO.OUT)
+  #  GPIO.setup(8, GPIO.OUT)
+  #  GPIO.setup(9, GPIO.OUT)
+  #  GPIO.setup(10, GPIO.OUT)
     # Turn all motors off
-    GPIO.output(7, 0)
-    GPIO.output(8, 0)
-    GPIO.output(9, 0)
-    GPIO.output(10, 0)
+  #  GPIO.output(7, 0)
+  #  GPIO.output(8, 0)
+  #  GPIO.output(9, 0)
+  #  GPIO.output(10, 0)
 
     g_node = rclpy.create_node('motor')
 
@@ -73,7 +73,7 @@ def main(args=None):
         rclpy.spin_once(g_node)
 
     # Reset the GPIO pins (turns off motors too)
-    GPIO.cleanup()
+   # GPIO.cleanup()
 
     # Destroy the service attached to the node explicitly
     # (optional - otherwise it will be done automatically
